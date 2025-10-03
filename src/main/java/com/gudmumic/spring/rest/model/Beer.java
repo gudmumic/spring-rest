2ce97f6b92d3021d7a41eb6043175e284d202284
Michael Nielsen <gudmundseth.dk@gmail.com>
2025-10-03 21:24:47 +0200

    StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION fixed. Jackson need this constructor for serialization

--- src/main/java/com/gudmumic/spring/rest/model/Beer.java
+++ src/main/java/com/gudmumic/spring/rest/model/Beer.java
@@ -1,7 +1,9 @@
 package com.gudmumic.spring.rest.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -9,6 +11,8 @@ import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Beer {
     private UUID id;
     private Integer version;
@@ -19,6 +23,4 @@ public class Beer {
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
-
-
 }
