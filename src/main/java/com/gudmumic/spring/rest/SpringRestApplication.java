7b8297fb0e49e52503644c5a08a230c5a06dc4ec
Michael Nielsen <gudmundseth.dk@gmail.com>
2025-10-10 06:45:24 +0200

    configuration of mysql working, but not optimal

--- src/main/java/com/gudmumic/spring/rest/SpringRestApplication.java
+++ src/main/java/com/gudmumic/spring/rest/SpringRestApplication.java
@@ -3,10 +3,22 @@ package com.gudmumic.spring.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
 @SpringBootApplication
 public class SpringRestApplication {
 
 	public static void main(String[] args) {
+        Properties mysqlProps = new Properties();
+        try (FileReader reader = new FileReader("./src/main/resources/application-docker-mysql.properties")) {
+            mysqlProps.load(reader);
+            mysqlProps.forEach((key, value) -> System.setProperty(key.toString(), value.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 		SpringApplication.run(SpringRestApplication.class, args);
 	}
 
